403b20a27fbf6da44254b6f479c27ead5745573b
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-07 12:39:30 -0300

    Run migrate before start server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const routes = require('./src/routes/routes')
+const knex = require('./src/database/knex')
 // const factories = require('./test/factories/productFactory')
 // require('./src/crons/resseler')
 const jwt = require('jsonwebtoken')
@@ -19,8 +20,31 @@ console.log(token)
 app.use(express.json())
 
 app.use(routes)
+/*
+knex.migrate.rollback(null, true)
+    .then(() => {
+        console.log('Rollback finalizado!');
+    }).catch(err => {
+        console.error('Fudeu!!!!! ---------------------------');
+        console.error(err.message);
+    });
+*/
 
-app.listen(3336, ()=>{
-    console.log('Server Online, port: 3336')
-})
+function startServer() {
+    app.listen(3336, ()=>{
+        console.log('Server Online, port: 3336');
+    });
+}
 
+knex.migrate.latest()
+    .then(() => {
+        console.log('Migration finished!');
+        startServer();
+    })
+    .catch(err => {
+        console.error('--------------------------- Fudeu!!!');
+        console.error(err.message);
+
+        console.error('--------------------------- Rollback!!!');
+        knex.migrate.rollback();
+    });
