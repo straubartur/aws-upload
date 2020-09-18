1a21c1760bac1be8ca8a4e4c2d80925e44cb81ba
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-17 22:43:00 -0300

    Add removed column in tables by migration

---
+++ src/database/migrations/20200917223350_alter_users_table_add_is_removed_column.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex) {
+    return knex.schema.table('Users', function (table) {
+        table.boolean('is_removed').notNullable().defaultTo(false);
+        table.dateTime('removed_at');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.table('Users', function (table) {
+        table.dropColumn('is_removed');
+        table.dropColumn('removed_at');
+    });
+};
