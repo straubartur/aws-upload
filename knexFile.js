75ed2f19d3e1cbc8b1a560e41aed813794b9bfd1
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-22 12:25:29 -0300

    Fix user and password to knexFile

--- knexFile.js
+++ knexFile.js
@@ -4,10 +4,10 @@ module.exports = {
       client: 'mysql',
       connection: {
         host: process.env.DB_HOST,
-        port: '3306',
+        port: process.env.DB_PORT || 3306,
         user: process.env.DB_USER,
-        password: process.env.DB_DATABASE,
-        database: process.env.DB_PASSWORD
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
       },
       migrations: {
         tableName: 'knexMigrations',
@@ -17,6 +17,5 @@ module.exports = {
         directory: `${__dirname}/src/database/seeds`
     }
     }
-  
   };
   
