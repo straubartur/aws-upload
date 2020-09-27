2c305b066c8401e6717c5f1dff22ee008d151888
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-27 20:08:09 -0300

    Update migrate

--- src/database/migrations/20200927230139_alter_customers_table_add_aws_logo_path_column.js
+++ src/database/migrations/20200927230139_alter_customers_table_add_aws_logo_path_column.js
@@ -1,8 +1,12 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.table('Customers', (table)=>{
+        table.text('aws_logo_path');
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.table('Customers', (table)=>{
+        table.dropColumn('aws_logo_path');
+    });
 };
