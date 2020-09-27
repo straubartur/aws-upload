91c8c86e5e24af815cb9c30b8a65684b0f2fc393
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-27 20:31:45 -0300

    Update Packages to use transaction

--- src/controllers/PackagesController.js
+++ src/controllers/PackagesController.js
@@ -1,30 +1,33 @@
 const { buildMessage } = require('../utils/buildMessage');
-const packagesService = require('../services/PackagesService');
-const packageValidator = require('../validator/PackageValidator')
+const { getTransaction } = require('../database/knex');
+const PackagesService = require('../services/PackagesService');
+const packageValidator = require('../validator/PackageValidator');
 
 function getPackages (req, res) {
     const { limit, page } = req.query;
+    const packagesService = new PackagesService();
 
     packagesService.find(undefined, '*', { limit, page })
         .then(result => res.status(200).json(result))
         .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage(error.message));
+            console.log(error);
+            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
         });
 }
 
 function getPackageById(req, res) {
     const { id } = req.params;
+    const packagesService = new PackagesService();
 
     packagesService.findById(id)
         .then(result => res.status(200).json(result))
         .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage(error.message));
+            console.log(error);
+            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
         });
 }
 
-function savePackage (req, res) {
+async function savePackage (req, res) {
     const package = req.body || {};
 
     const { error } = packageValidator.Package.validate(package);
@@ -33,42 +36,70 @@ function savePackage (req, res) {
         return res.status(500).json(buildMessage('Ops! Algo deu errado =[', error));
     }
 
+    const trx = await getTransaction();
+    const packagesService = new PackagesService(trx);
+
     packagesService.save(package)
-        .then(() => res.status(201).json(buildMessage('Package salvo com sucesso')))
+        .then(() => {
+            trx.commit();
+            res.status(201).json(buildMessage('Package salvo com sucesso'))
+        })
         .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage(error.message));
+            console.log(error);
+            trx.rollback();
+            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
         });
 }
 
-function deletePackage (req, res) {
+async function deletePackage (req, res) {
     const { id } = req.params;
-    
+
+    const trx = await getTransaction();
+    const packagesService = new PackagesService(trx);
+
     packagesService.deleteById(id)
-        .then(() => res.status(204).json(buildMessage('Package deletado com sucesso', { id })))
+        .then(() => {
+            trx.commit();
+            res.status(204).json(buildMessage('Package deletado com sucesso', { id }))
+        })
         .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage(error.message));
+            console.log(error);
+            trx.rollback();
+            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
         });
 }
 
-function publishPackage(req, res) {
+async function publishPackage(req, res) {
     const { id } = req.params;
+
+    const trx = await getTransaction();
+    const packagesService = new PackagesService(trx);
 
     packagesService.publishPackage(id)
-        .then(() => res.status(200).json(buildMessage('Package publicado com sucesso', { id })))
+        .then(() => {
+            trx.commit();
+            res.status(200).json(buildMessage('Package publicado com sucesso', { id }))
+        })
         .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage(error.message));
+            console.log(error);
+            trx.rollback();
+            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
         });
 }
 
 function generateUrls(req, res) {
-   const { packageId } = req.query;
-   const contentTypeList = req.body || [];
+    const { packageId } = req.query;
+    const contentTypeList = req.body || [];
+
+    const packagesService = new PackagesService();
 
     packagesService.generateUrls(packageId, contentTypeList)
         .then(uploadURLs => res.status(200).json(uploadURLs))
+        .catch(error => {
+            console.log(error);
+            trx.rollback();
+            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
+        });
 }
 
 module.exports = {
--- src/validator/PackageValidator.js
+++ src/validator/PackageValidator.js
@@ -5,6 +5,8 @@ const Package = Joi.object({
     id: Joi.string().guid({version: 'uuidv4'}).required(),
     name: Joi.string().required(),
     description: Joi.string(),
+    is_published: Joi.valid(0, 1),
+    end_date: Joi.date().iso().allow(null),
     created_at: Joi.date().iso().allow(null),
     updated_at: Joi.date().iso().allow(null),
     is_removed: Joi.valid(0, 1),
