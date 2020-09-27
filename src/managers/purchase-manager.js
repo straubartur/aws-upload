54d68d9481d749471b945b8f457a4b989096465d
Edinho <bruno.monteirodg@gmail.com>
2020-09-27 19:40:44 -0300

    feat: added function syncPurchasePosts and existsPurchasePost

--- src/managers/purchase-manager.js
+++ src/managers/purchase-manager.js
@@ -1,5 +1,8 @@
 const purchasePostsService = require('../services/PurchasePostsSevice')
+const packagesPostsService = require('../services/PackagePostsService')
 const rules = require('./purchase-rules')
+const uuid = require('uuid')
+const mime = require('mime-types')
 
 /**
  * @typedef { import('../externals/watermark').Purchase } Purchase
@@ -46,6 +49,63 @@ async function rule (dataRule, fnValidate) {
     }
 }
 
+/**
+ * Sync the values from Packages_posts to Purchase_posts
+ * @param { Purchase } purchase
+ * @return { Promise<Boolean> }
+ */
+async function syncPurchasePosts (purchase) {
+    const isValid = await validatePurchase(purchase)
+
+    if (!isValid) {
+        return false
+    }
+
+    const posts = await packagesPostsService.findByPackageId(purchase.package_id)
+
+    if (posts.data && !posts.data.length) {
+        return false
+    }
+
+    posts.data.forEach(async post => {
+        const postExists = await existsPurchasePost(purchase.id, post.id)
+
+        if (!postExists) {
+            const id = uuid.v4()
+            let extension = mime.extension(mime.lookup(post.aws_path))
+            extension = extension ? '.' + extension : ''
+            /**
+             * @type { import('../services/PurchasePostsSevice').PurchasePost }
+             */
+            const newPost = {
+                id,
+                package_post_id: post.id,
+                purchase_id: purchase.id,
+                aws_path: `purchases/${purchase.id}/posts/${id}${extension}`,
+                aws_path_thumb: `purchases/${purchase.id}/posts/thumb-${id}${extension}`,
+                watermark_status: '',
+                is_removed: 0,
+                created_at: new Date()
+            }
+            await purchasePostsService.create(newPost)
+        }
+    })
+
+    return true
+}
+
+/**
+ * Check if the purchase post exists in database
+ * @param { string } purchase_id - The puchase id
+ * @param { string } package_post_id - The packagePost id
+ * @return { Promise<Boolean> }
+ */
+async function existsPurchasePost (purchase_id, package_post_id) {
+    const post = await purchasePostsService.find({ purchase_id, package_post_id, is_removed: 0 })
+    return Boolean(post.data.length)
+}
+
 module.exports = {
-    validatePurchace
+    validatePurchase,
+    syncPurchasePosts
 }
