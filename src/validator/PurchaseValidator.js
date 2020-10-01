b9a3d2285ae4455424d314c376f44c468d8051db
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-09-30 22:53:47 -0300

    Add purchase validator

---
+++ src/validator/PurchaseValidator.js
@@ -0,0 +1,20 @@
+const Joi = require('joi');
+
+const PurchaseValidator = Joi.object({
+    id: Joi.string().guid({version: 'uuidv4'}),
+    customer_id: Joi.string().guid({version: 'uuidv4'}).required(),
+    package_id: Joi.string().guid({version: 'uuidv4'}).required(),
+    loja_integrada_pedido_id: Joi.number().integer(),
+    is_paid: Joi.valid(0, 1, false, true),
+    custom_name: Joi.string().required(),
+    custom_phone: Joi.string().required(),
+    rank: Joi.string().required(),
+    aws_logo_path: Joi.string().required(),
+    watermark_status: Joi.string(),
+    created_at: Joi.date().iso().allow(null),
+    updated_at: Joi.date().iso().allow(null),
+    is_removed: Joi.valid(0, 1, false, true),
+    removed_at: Joi.date().iso().allow(null)
+});
+
+module.exports = PurchaseValidator;
