17fef861963506e9b1cca2262b26417a523cf965
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-10-01 00:21:08 -0300

    Add front in project

---
+++ public/precache-manifest.6d023fe5915cf4be0ba24f65902609fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba55e22b07c57152893d2ab648255a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e301e5bbdc1049a2cdd8",
+    "url": "/static/css/main.543bf164.chunk.css"
+  },
+  {
+    "revision": "ffa00344cdf7447ced61",
+    "url": "/static/js/2.9fd4d301.chunk.js"
+  },
+  {
+    "revision": "151a69d5b7d1042933df0d91022f926d",
+    "url": "/static/js/2.9fd4d301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e301e5bbdc1049a2cdd8",
+    "url": "/static/js/main.b70c9033.chunk.js"
+  },
+  {
+    "revision": "43615a57423f6e9c4640",
+    "url": "/static/js/runtime-main.15198f90.js"
+  },
+  {
+    "revision": "5a7cbd3475355de70c35146e7c774006",
+    "url": "/static/media/unnamed.5a7cbd34.png"
+  }
+]);
