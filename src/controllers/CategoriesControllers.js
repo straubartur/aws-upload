3f537971bf9ae9d6202e6f5e0e0aa22a1ea8d0b1
Artur Straub <straubartur@gmail.com>
2021-02-11 10:02:40 -0300

    Update CategoriesControllers.js

--- src/controllers/CategoriesControllers.js
+++ src/controllers/CategoriesControllers.js
@@ -1,111 +1,102 @@
+
 const CategoriesService = require('../services/CategoriesService');
 const categoryValidator = require('../validator/CategoryValidator')
 const { buildMessage } = require('../utils/buildMessage');
 const { getTransaction } = require('../database/knex');
 
-function getCategories(req, res) {
-    const { pagination, limit, page } = req.query;
-    const usePagination = pagination === 'false' ? false : true;
-
-    const categoriesService = new CategoriesService();
-
-    categoriesService
-        .find(undefined, '*', {
-            pagination: usePagination,
-            limit,
-            page
-        })
-        .then(result => res.status(200).json(result))
-        .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
-        });
-}
-
-function getCategoryById(req, res) {
-    const { id } = req.params;
-
-    const categoriesService = new CategoriesService();
-
-    categoriesService.findById(id)
-        .then(result => res.status(200).json(result))
-        .catch(error => {
-            console.log(error)
-            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
-        });
-}
-
-async function createCategory(req, res) {
-    const newCategory = req.body || {};
-
-    const { error } = categoryValidator.validate(newCategory);
-
-    if (error) {
-        return res.status(500).json(buildMessage('Ops! Algo deu errado =[', error));
+function getById(Service) {
+    return async function getById(req, res) {
+        const { id } = req.params;
+    
+        Service.findById(id)
+            .then(result => res.status(200).json(result))
+            .catch(error => {
+                console.log(error)
+                res.status(500).json(buildMessage('Ops! Algo deu errado =['));
+            });
     }
-
-    const trx = await getTransaction();
-    const categoriesService = new CategoriesService(trx);
-
-    categoriesService.create(newCategory)
-        .then(category => {
-            trx.commit();
-            res.status(201).json(buildMessage('Categoria criada com sucesso', { id: category.id }))
-        })
-        .catch(error => {
-            console.log(error);
-            trx.rollback();
-            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
-        });
 }
 
-async function updateCategory(req, res) {
-    const { id } = req.params;
-    const category = req.body || {};
+async function deleteBy(Service) {
+    return async function(req, res) {
+        const { id } = req.params;
 
-    const { error } = categoryValidator.validate(category);
+        const trx = await getTransaction();
 
-    if (error) {
-        return res.status(500).json(buildMessage('Ops! Algo deu errado =[', error));
+        Service.deleteById(id)
+            .then(() => {
+                trx.commit();
+                res.sendStatus(204);
+            })
+            .catch(error => {
+                console.log(error);
+                trx.rollback();
+                res.status(500).json(buildMessage('Ops! Algo deu errado =['));
+            });
+        }
+}
+
+async function create(Service, validator) {
+    return async function(req, res) {
+        const { body } = req.body || {};
+    
+        const { error } = validator.validate(body);
+    
+        if (error) {
+            return res.status(500).json(buildMessage('Ops! Algo deu errado =[', error));
+        }
+    
+        const trx = await getTransaction();
+    
+        Service.create(body)
+            .then(category => {
+                trx.commit();
+                res.status(201).json(buildMessage('Categoria criada com sucesso', { id: category.id }))
+            })
+            .catch(error => {
+                console.log(error);
+                trx.rollback();
+                res.status(500).json(buildMessage('Ops! Algo deu errado =['));
+            });
     }
-
-    const trx = await getTransaction();
-    const categoriesService = new CategoriesService(trx);
-
-    categoriesService.updateById(id, category)
-        .then(() => {
-            trx.commit();
-            res.status(200).json(buildMessage('Categoria modificada com sucesso', { id }))
-        })
-        .catch(error => {
-            console.log(error);
-            trx.rollback();
-            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
-        });
 }
 
-async function deleteCategory(req, res) {
-    const { id } = req.params;
 
-    const trx = await getTransaction();
-    const categoriesService = new CategoriesService(trx);
-
-    categoriesService.deleteById(id)
-        .then(() => {
-            trx.commit();
-            res.sendStatus(204);
-        })
-        .catch(error => {
-            console.log(error);
-            trx.rollback();
-            res.status(500).json(buildMessage('Ops! Algo deu errado =['));
-        });
+function update(Service, validator) {
+    return async function (req, res) {
+        const { id } = req.params;
+        const { body } = req.body || {};
+    
+        const { error } = validator.validate(body);
+    
+        if (error) {
+            return res.status(500).json(buildMessage('Ops! Algo deu errado =[', error));
+        }
+    
+        const trx = await getTransaction();
+    
+        Service.updateById(id, body)
+            .then(() => {
+                trx.commit();
+                res.status(200).json(buildMessage('Categoria modificada com sucesso', { id }))
+            })
+            .catch(error => {
+                console.log(error);
+                trx.rollback();
+                res.status(500).json(buildMessage('Ops! Algo deu errado =['));
+            });
+    }
 }
 
-module.exports = {
-    getCategories,
-    getCategoryById,
-    createCategory,
-    updateCategory,
-    deleteCategory
-};
+
+
+module.exports = (() => {
+    const categoryService = new CategoriesService(trx)
+    return {
+        getCategories: get(categoryService),
+        getCategoriesById: getById(categoryService),
+        createCategory: create(categoryService, categoryValidator),
+        updateCategory: update(categoryService, categoryValidator),
+        deleteCategory: deleteBy(categoryService)
+    }
+})();
